c4f209fb1abd6e68ac8711863cc402d49590ab15
ShivPatel031 <patelshiv3123@gmail.com>
2024-11-22 15:30:18 +0530

    change welcome route

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 
-app.get('/api',(req,res)=>{return res.status(200).json(
+app.get('/',(req,res)=>{return res.status(200).json(
     {
         success:true,
-        message:"This is School Management API"
+        message:"Welcome to School Management API"
     }
 )});
 
