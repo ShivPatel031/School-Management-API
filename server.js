8aa60f4cfbf097d03ec8dcf61357162487aeb6e6
ShivPatel031 <patelshiv3123@gmail.com>
2024-12-22 15:13:00 +0530

    minor change to bodyparser

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 8001;
 //middleware
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     return res.status(200).json(
